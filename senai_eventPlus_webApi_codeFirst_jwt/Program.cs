4bdea86e77e06e7c1fe0b70c93ad215e1d2d0b66
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2023-10-14 09:27:11 -0300

    Inserindo comentarios XML no swagger.

--- senai_eventPlus_webApi_codeFirst_jwt/Program.cs
+++ senai_eventPlus_webApi_codeFirst_jwt/Program.cs
@@ -1,11 +1,49 @@
+﻿using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using senai_eventPlus_webApi_codeFirst_jwt.Controllers;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services
+    .AddControllers()
+    .AddNewtonsoftJson(options =>
+    {
+        // Ignora os loopings nas consultas
+        options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+        // Ignora valores nulos ao fazer jun��es nas consultas
+        options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "API Event Plus",
+        Description = "Api responsável pelo gerenciamento de um sistema, onde seja possível realizar a gestão de eventos na escola.",
+        //TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Allan Rodrigues",
+            Email = "allanrodrigues1991.ar@gmail.com",
+            Url = new Uri("https://github.com/AllanR1991")
+        },
+        /*License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }*/
+    });
+
+    // using System.Reflection;
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 var app = builder.Build();
 
